e9aee52f2830643104f73f20478b445bf8d0dff2
Slartron <33701252+Slartron@users.noreply.github.com>
2024-05-16 14:59:20 +0200

    Change directory structure and add preview for C# 9

---
+++ Ver9preview/NewLanguageFeatures.cs
@@ -0,0 +1,12 @@
+﻿namespace Ver9Preview;
+
+internal class NewLanguageFeatures
+{
+    internal void EscapeSequenceForESC()
+    {
+        var text1 = $"\e";
+        var text2 = "\u001b";
+        Console.WriteLine("\\e equals \\u001b: {0}", text1 == text2); // True
+        Console.WriteLine("Value of \\e: {0}", (int)text1[0]); // 27
+    }
+}
---
+++ Ver9preview/Program.cs
@@ -0,0 +1,6 @@
+﻿using Ver9Preview;
+
+NewLanguageFeatures langFeatures = new();
+
+langFeatures.EscapeSequenceForESC();
+
